5754cf397818785a74dac36460af930542735ec3
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-30 03:27:54 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -28,7 +28,7 @@ WIN_COMBINATIONS = [
 
   def won?
     WIN_COMBINATIONS.detec do |winner|
-      @board.cells[winner[0]]
+      @board.cells[winner[0]] == @board.cells[winner[1]]
       [1, 4, 7]
     end
   end
