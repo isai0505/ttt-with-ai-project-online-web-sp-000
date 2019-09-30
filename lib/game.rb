aa28af93b4d9263584c7a680252d7bfd78e370f5
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-30 04:15:13 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -59,4 +59,15 @@ WIN_COMBINATIONS = [
       end
     end
 
+    def play
+      while !over?
+        turn
+      end
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end
+    end
+
 end
