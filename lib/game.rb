d536de845ec18c1bc8183fd93e2cac2a7f18f046
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-30 03:56:34 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -27,6 +27,7 @@ WIN_COMBINATIONS = [
     WIN_COMBINATIONS.detect do |winner|
       @board.cells[winner[0]] == @board.cells[winner[1]] &&
       @board.cells[winner[1]] == @board.cells[winner[2]]
+      @board.cells[winner[0]] == "X"
     end
   end
 
